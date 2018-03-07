65d47744f27b0011d9ef6d07c4260298395bfc4a
Samuel <terefesam@gmail.com>
2018-03-06 19:53:15 -0800

    feat: finished setting up the server npm installed all the required dependencies and launched server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+const mustacheExpressInstance = mustacheExpress();
+mustacheExpressInstance.cache = null; // modify template with out restarting
+app.engine('mustache', mustacheExpressInstance);
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res){
+    res.send('Hello');
+});
+
+app.listen(3000, function(){
+    console.log('listening on port 3000')
+})
