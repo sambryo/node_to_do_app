4b40cfd68c8ccf0bd8f87951c009f689c838d7f5
Samuel <terefesam@gmail.com>
2018-03-07 12:47:38 -0800

    feat: able to list the items

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,17 @@ const router = require('express').Router();
 const Todo = require('../models/todo');
 
 router.get('/', function(req, res){
-    res.render('index', {})
+    Todo.find({}).then(function(results){
+        res.render('index', {todos: results})
+    });
 });
 
 router.post('/todos', function(req, res){
-    res.json(req.body);
-})
+    let newTodo = new Todo({description: req.body.description});
+    newTodo.save().then(function(result){
+        console.log(result);
+        res.redirect('/');
+    });
+});
 
 module.exports = router;
