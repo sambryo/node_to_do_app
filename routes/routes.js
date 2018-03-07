f5de4331675ab9172d25aec0edd9521e895b8871
Samuel <terefesam@gmail.com>
2018-03-07 13:12:42 -0800

    feat: Done is working

--- routes/routes.js
+++ routes/routes.js
@@ -19,8 +19,15 @@ router.post('/todos', function(req, res){
 });
 
 router.post('/todos/:id/completed', function(req, res){
-    console.log(req.params)
-    res.redirect('/');
+    let todoId = req.params.id;
+    Todo.findById(todoId)
+        .exec()
+        .then(function(result){
+            result.done = !result.done;
+            return result.save();
+        }).then(function(result){
+            res.redirect('/');
+        });
 });
 
 module.exports = router;
